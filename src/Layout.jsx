4208494842e0b4b7d217eab961c325cc514fe028
bhupesh jha <shrutijha0803@gmail.com>
2025-03-21 12:52:55 +0530

    removed user

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import Footer from "./Components/Footer/Footer";
-import Header from "./src/Components/Header/header";
 import { Outlet } from "react-router-dom";
+import Header from "./Components/Header/Header"
+import Footer from "./Components/Footer/Footer";
 function Layout() {
     return(
         <>
-        <Header />
+        <Header>
+        </Header>
         <Outlet />
         <Footer />
         </>
