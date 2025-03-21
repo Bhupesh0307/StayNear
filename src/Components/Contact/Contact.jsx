1e854749e8beeffe9a44bc4431276d55d65c4762
bhupesh jha <shrutijha0803@gmail.com>
2025-03-21 13:10:33 +0530

    log innnn check

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -131,6 +131,7 @@ export default function Contact() {
                             <button
                                 type="submit"
                                 className="md:w-32 bg-orange-700 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-orange-600 transition ease-in-out duration-300"
+                                onClick={Contact}
                             >
                                 Submit
                             </button>
