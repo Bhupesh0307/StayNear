39bd0aa007803130b6d44bed4f2e8add8dbac237
bhupesh jha <shrutijha0803@gmail.com>
2025-04-03 02:48:26 +0530

    fixing submit button error

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { websiteName } from "../Home/Home";
 
 export default function Contact() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Form Submitted!");
+        alert("Thank you for contacting us!");
+    };
     return (
         <div className="relative flex items-top justify-center min-h-[700px] bg-white sm:items-center sm:pt-0">
             <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
@@ -19,20 +24,20 @@ export default function Contact() {
                                 <svg
                                     fill="none"
                                     stroke="currentColor"
-                                    stroke-linecap="round"
+                                    strokeLinecap="round"
                                     stroke-linejoin="round"
                                     stroke-width="1.5"
                                     viewBox="0 0 24 24"
                                     className="w-8 h-8 text-gray-500"
                                 >
                                     <path
-                                        stroke-linecap="round"
+                                        strokeLinecap="round"
                                         stroke-linejoin="round"
                                         stroke-width="1.5"
                                         d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
                                     />
                                     <path
-                                        stroke-linecap="round"
+                                        strokeLinecap="round"
                                         stroke-linejoin="round"
                                         stroke-width="1.5"
                                         d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
@@ -47,14 +52,14 @@ export default function Contact() {
                                 <svg
                                     fill="none"
                                     stroke="currentColor"
-                                    stroke-linecap="round"
+                                    strokeLinecap="round"
                                     stroke-linejoin="round"
                                     stroke-width="1.5"
                                     viewBox="0 0 24 24"
                                     className="w-8 h-8 text-gray-500"
                                 >
                                     <path
-                                        stroke-linecap="round"
+                                        strokeLinecap="round"
                                         stroke-linejoin="round"
                                         stroke-width="1.5"
                                         d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
@@ -69,14 +74,14 @@ export default function Contact() {
                                 <svg
                                     fill="none"
                                     stroke="currentColor"
-                                    stroke-linecap="round"
+                                    strokeLinecap="round"
                                     stroke-linejoin="round"
                                     stroke-width="1.5"
                                     viewBox="0 0 24 24"
                                     className="w-8 h-8 text-gray-500"
                                 >
                                     <path
-                                        stroke-linecap="round"
+                                        strokeLinecap="round"
                                         stroke-linejoin="round"
                                         stroke-width="1.5"
                                         d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
@@ -88,9 +93,9 @@ export default function Contact() {
                             </div>
                         </div>
 
-                        <form action="https://stay-near.vercel.app/about" className="p-6 flex flex-col justify-center">
+                        <form onSubmit={handleSubmit} className="p-6 flex flex-col justify-center">
                             <div className="flex flex-col">
-                                <label for="name" className="hidden">
+                                <label htmlFor="name" className="hidden">
                                     Full Name
                                 </label>
                                 <input
